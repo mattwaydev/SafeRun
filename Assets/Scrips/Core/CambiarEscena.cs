50e69a1788dc6b3372e34104e5caae085c2d439e
Jlang27 <joseluisangulom27@gmail.com>
2026-04-05 20:49:08 -0500

    Nuevo Script: CambiarEscena. Posibilidad de cambiar de escena desde la pantalla menú, falta elaborar las demás escenas como opciones e implementar la opción de salir.

---
+++ Assets/Scrips/Core/CambiarEscena.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CambiarEscena : MonoBehaviour
+{
+        [SerializeField] private string Escena;
+        public void CambioEscena()
+    {
+        SceneManager.LoadScene(Escena);
+    }
+}
